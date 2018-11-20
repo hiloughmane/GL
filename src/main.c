ea314d6ea5580e3aedace645c8bec0b0bab0506c
hajar iloughmane <hajar.iloughmane@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:47:56 +0100

    j'ai terminé la question 2

--- src/main.c
+++ src/main.c
@@ -13,3 +13,36 @@ void main(void)
 		else bin[i] = 0;
 	}
 }
+
+
+
+// exercice 56 question 2:
+
+
+#include <stdlib.h>
+#define SEUILB -100
+#define SEUILH 200
+void main(void)
+
+{
+
+ unsigned int sat[10];
+ unsigned char i;
+ short rand_a_b (short a = -100 , short b = 200)
+ short nb_aleatoire = 0;
+ 
+ for ( i = 0; i<10 ; i++ )
+ {
+	 nb_aleatoire = rand()%( b - a )+ a ;
+     sat[i] = nb_aleatoire;
+ }
+}
+	   
+
+
+
+
+
+
+
+
