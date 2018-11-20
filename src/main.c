81a3f05c31d9a7bcd3c9c4c0d5d6b7cb30559755
hiloughmane <44801693+hiloughmane@users.noreply.github.com>
2018-11-20 20:19:25 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -28,12 +28,12 @@ void main(void)
 
  unsigned int sat[10];
  unsigned char i;
- short rand_a_b (short a = -100 , short b = 200)
+ short rand_SEUILB_SEUILH ( SEUILB , SEUILH );
  short nb_aleatoire = 0;
  
  for ( i = 0; i<10 ; i++ )
  {
-	 nb_aleatoire = rand()%( b - a )+ a ;
+	 nb_aleatoire = rand()%( SEUILH - SEUILB ) + SEUILH;
      sat[i] = nb_aleatoire;
  }
 }
