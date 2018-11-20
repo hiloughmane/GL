30a9c0270cb643506708de6156bc94cd0abd1c49
hajar iloughmane <hajar.iloughmane@ge1.iut-cachan.u-psud.fr>
2018-11-20 11:28:21 +0100

    j'ai terminé la question 1 de l'exo56

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
-#include "my_header.h"
-
-int main(void)
+void main(void)
 {
-    return 0;
+	unsigned char bin[10] = {0,1,2,3,4,5,6,7,8,9} ;
+	unsigned char seuil = 5;
+	unsigned char i;
+	
+	
+	
+	for ( i=0 ; i<10 ; i++ )
+	{ 
+		if ( bin[i]> seuil )
+			bin[i] = 1;
+		else bin[i] = 0;
+	}
 }
