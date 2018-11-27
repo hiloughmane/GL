bc43072dd41f0dcd35a2cd4b754dc14ce7c5ef8b
hiloughmane <44801693+hiloughmane@users.noreply.github.com>
2018-11-27 23:25:01 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <stdlib.h>
+#include <my.header.h>
+#include <TP_lib_suite.h>
+ 
 
-#include "my_header.h"
-
-int main(void)
+void main(void)
 {
-    return 0;
+    unsigned char nb_bons_nums = 0;
+    unsigned int comb_win[6];
+    unsigned int comb_play[6] = {2,5,7,9,10,15}.
+    short tirerNumero(void);
+    short comb_aleatoire;
+    void initialiserTirage(void);
+    unsigned char i , j , n;
+    
+    for ( n = 0 ; n < 100 ; n++ )
+    {
+         initialiserTirage();
+         for ( i = 0; i < 6; i++ )
+           {
+             for ( j = 0 ; j < 6 ; j++ )
+               {
+                 comb_aleatoire = tirerNumero;
+                 comb_win[j] = comb_aleatoire;
+                  if ( comb_win[j] == comb_play[i] )
+                      nb_bons_nums = nb_bons_nums + 1;
+               }
+           }
+    }
 }
