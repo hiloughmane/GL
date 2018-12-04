e232d97642b197ac70d0103af0c9163909e16ed3
hiloughmane <44801693+hiloughmane@users.noreply.github.com>
2018-12-04 21:16:18 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,22 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
+#include "stdio.h"
 
-int main(void)
-{
-    return 0;
+void main(void)
+ {
+    unsigned int age = 0;
+    char prenom[100];
+    char phrase[100];
+    
+    printf("tapez le prenom: ");
+    scanf("%s", prenom);
+    
+    printf("tapez l'age: ");
+    scanf("%d", &age);
+    
+    sprintf(phrase, "je m'appelle %s et j'ai %d ans", prenom, age);
+    printf("%s", phrase);
+    
+    
+    
+    getchar()
 }
